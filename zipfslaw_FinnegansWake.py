34f410d61e2421344b15bcb73af999b46adde954
mayukhdifferent <mayukhdifferent@gmail.com>
2017-04-16 10:15:41 +0530

    Add files via upload

---
+++ zipfslaw_FinnegansWake.py
@@ -0,0 +1,11 @@
+import re
+from operator import itemgetter
+frequency = {}
+open_file = open('FinnegansWake.txt', 'r')
+file_to_string = open_file.read()
+words = re.findall(r'(\b[A-Za-z][a-z]{2,109}\b)', file_to_string)
+for word in words:
+    count = frequency.get(word,0)
+    frequency[word] = count + 1    
+for key, value in reversed(sorted(frequency.items(), key = itemgetter(1))):
+    print key, value
